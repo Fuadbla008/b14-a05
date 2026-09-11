c2635c27f6c4e4341e58a53a8fe92cd51e243fb8
Fuadbla008 <mdbarkatislamfuad@gmail.com>
2026-09-11 17:09:35 +0600

    added fetched data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { Suspense, use } from 'react';
 import './App.css'
 import Footer from './component/Footer';
 import HeroSec from './component/HeroSec';
 import Navber from './component/Navber';
+
+const fetchdata = async() =>{
+  const res = await fetch('/data.json')
+  const data = await res.json();
+  return data;
+}
+const stateData = fetchdata();
+
+const Stack = () =>{
+  const stackName = use(stateData);
+  const getStack = stackName.length;
+  return console.log(getStack);
+}
 
 function App() {
 
@@ -10,6 +24,11 @@ function App() {
       <div className="container mx-auto py-5 grid gap-10">
         <Navber></Navber>
         <HeroSec></HeroSec>
+
+        <Suspense fallback="loading">
+          <Stack></Stack>
+        </Suspense>
+
         <Footer></Footer>
       </div>
     </div>
